c3e59629bd8d9f437b2fbb4fadff48f60c1192b3
Nicolò Verardo <nicoloverardo@users.noreply.github.com>
2020-10-09 17:40:22 +0200

    Now pruning embeddings

---
+++ text.py
@@ -0,0 +1,17 @@
+import nltk
+import string
+
+class PreProcessing():
+    @staticmethod
+    def cleanText(x):
+        tokens = nltk.word_tokenize(x)
+        tokens = [w.lower() for w in tokens]
+        words = [word for word in tokens if word.isalpha()]
+        table = str.maketrans('', '', string.punctuation)
+        stripped = [w.translate(table) for w in tokens]
+        stopwords = nltk.corpus.stopwords.words('english')
+        stopwords.extend(string.punctuation)
+        words = [w for w in stripped if w.isalpha() and not w in stopwords]
+        words = [w for w in words if len(w) > 1 and w.isalpha()]
+    
+        return " ".join(words)  
